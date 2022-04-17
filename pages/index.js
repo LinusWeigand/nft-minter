244c4cceeeb76bf816793f325cd14fa52af8082d
Linus Weigand <linusweigand@protonmail.com>
2022-04-17 18:11:12 +0200

    Added Button to connect Metamask

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import { useState, useEffect } from "react";
+import PrimaryButton from "../components/primary-button";
+
 export default function Home() {
+
+  const [ethereum, setEthereum] = useState(undefined);
+  const [connectedAccount, setConnectedAccount] = useState(undefined);
+
+  const connectAccount = () => {
+
+  };
+
+  if(!ethereum) {
+    return <p>Please install Metamask to connect to the site</p>
+  }
+
+  if(!connectedAccount) {
+    return <PrimaryButton onClick={connectAccount}>Connect Metamask Wallet</PrimaryButton>
+  }
+
   return <p>The start of our app!</p>
 }
