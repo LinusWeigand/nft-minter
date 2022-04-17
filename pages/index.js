f1d38e07f59af2c18936a3c0f08ed36f0c092b94
Linus Weigand <linusweigand@protonmail.com>
2022-04-17 18:15:59 +0200

    added dotenv

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,29 @@ export default function Home() {
   const [ethereum, setEthereum] = useState(undefined);
   const [connectedAccount, setConnectedAccount] = useState(undefined);
 
+  const handleAccounts = (accounts) => {
+    if (accounts.length > 0) {
+      const accoutn = accounts[0];
+      console.log("We have an authorized account: ", account);
+      setConnectedAccount(accounts);
+    } else {
+      console.log("No authorized account yet");
+    }
+  };
+
+  const getConnectedAccount = async () => {
+    if (window.ethereum) {
+      setEthereum(window.ethereum);
+    }
+
+    if (ethereum) {
+      const accounts = await ethereum.request({method: 'eth_accounts '});
+      handleAccounts(accounts);
+    }
+  };
+
+  useEffect(() => getConnectedAccount(), []);
+
   const connectAccount = () => {
 
   };
