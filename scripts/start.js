12095caa8031303e49788a22c4fba275fd43f69e
Linus Weigand <linusweigand@protonmail.com>
2022-04-17 17:55:06 +0200

    test as somebody else

--- scripts/start.js
+++ scripts/start.js
@@ -1,18 +1,22 @@
 const main = async () => {
-const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
-  const keyboardsContract = await keyboardsContractFactory.deploy();
-  await keyboardsContract.deployed();
+    const [owner, somebodyElse] = await hre.ethers.getSigners();
+    const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
+    const keyboardsContract = await keyboardsContractFactory.deploy();
+    await keyboardsContract.deployed();
 
-  console.log("Contract deployed to:", keyboardsContract.address);
+    console.log("Contract deployed to:", keyboardsContract.address);
 
-  let keyboards = await keyboardsContract.getKeyboards();
-  console.log("We got the keyboards!", keyboards);
+    let keyboards = await keyboardsContract.getKeyboards();
+    console.log("We got the keyboards!", keyboards);
 
-  const keyboardTxn = await keyboardsContract.create("A really great keyboard!");
-  await keyboardTxn.wait();
+    const keyboardTxn = await keyboardsContract.create("A really great keyboard!");
+    await keyboardTxn.wait();
 
-  keyboards = await keyboardsContract.getKeyboards();
-  console.log("We got the keyboards!", keyboards);
+    keyboards = await keyboardsContract.getKeyboards();
+    console.log("We got the keyboards!", keyboards);
+
+    keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
+    console.log("And as somebody else", keyboards);
 }
 
 const runMain = async () => {
